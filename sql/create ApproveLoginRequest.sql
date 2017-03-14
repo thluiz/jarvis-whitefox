bdda73989c59b9aced53f13ed3c31ad9e32e80f6
Thiago Luiz Silva <contato@thluiz.com>
2017-03-14 09:38:27 -0300

    final configurations for azure and local debugging

---
+++ sql/create ApproveLoginRequest.sql
@@ -0,0 +1,29 @@
+alter procedure ApproveLoginRequest(@temporaryToken varchar(128)) 
+as
+begin 
+	declare @providerId int, @providerKey varchar(128), @userId int, 
+		@details varchar(max), @name varchar(200)
+	
+	select @providerId = providerId, @providerKey = providerKey, 
+		@userId = u.userId, @details = details, @name = u.[name]
+	from loginrequest l
+		join users u on u.userid = l.userid 
+	where temporaryProviderKey = @temporaryToken
+
+	if(@userId is null)
+	begin
+		select 0 success, 'Usuario não encontrado' [Message]
+		for json path
+	end
+	else 
+	begin 
+		select 1 success, @details [Address],  
+		@name [Name], @providerKey Token
+		for json path
+	end
+
+end
+
+go
+
+ApproveLoginRequest 'hLhxsPP3emPBQn9FfLVhW0GTUhnB6ssl'
