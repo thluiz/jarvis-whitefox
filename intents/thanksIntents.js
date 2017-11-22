242cb93743aeeaa51748d597a4bf958b22599cbc
Thiago Luiz Silva <contato@thluiz.com>
2017-11-22 00:20:15 -0200

    pequenos ajustes promove

--- intents/thanksIntents.js
+++ intents/thanksIntents.js
@@ -1,11 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const intentBase_1 = require("./intentBase");
+const funnyMessages_1 = require("../domain/services/templates/funnyMessages");
 class ThanksIntents extends intentBase_1.IntentBase {
     setup(dialog) {
         dialog.matches("thanks", [
             (session, args, next) => {
-                session.endDialog("disponha!");
+                session.endDialog(funnyMessages_1.FunnyMessages.thankYouResponse());
             },
         ]);
     }
--- intents/thanksIntents.ts
+++ intents/thanksIntents.ts
@@ -1,11 +1,12 @@
 import * as builder from "botbuilder";
 import { IntentBase } from "./intentBase";
+import { FunnyMessages } from "../domain/services/templates/funnyMessages";
 
 export class ThanksIntents extends IntentBase {
     public setup(dialog: builder.IntentDialog): void {
         dialog.matches("thanks", [
-            (session, args, next) => {
-                session.endDialog("disponha!");
+            (session, args, next) => {                
+                session.endDialog(FunnyMessages.thankYouResponse());
             },
         ]);
     }
